aac26fcf9c2607c1ef90cdd65b7089ae7e66d103
guifroes1984 <guilhermefroes26@gmail.com>
2024-11-01 16:39:07 -0300

    Criado a entidade Area e o repository do mesmo, e inserido alguns dados.

---
+++ src/main/java/br/com/guifroes1984/agenda/domain/entities/Area.java
@@ -0,0 +1,55 @@
+package br.com.guifroes1984.agenda.domain.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_AREA")
+public class Area implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Area other = (Area) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/br/com/guifroes1984/agenda/domain/repositories/AreaRepository.java
@@ -0,0 +1,9 @@
+package br.com.guifroes1984.agenda.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.guifroes1984.agenda.domain.entities.Area;
+
+public interface AreaRepository extends JpaRepository<Area, Integer> {
+
+}
