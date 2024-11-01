c03b966e6a5850948ce325cfc2dd3464e21b3373
guifroes1984 <guilhermefroes26@gmail.com>
2024-11-01 16:21:13 -0300

    Fazendo mapeamento Usuario e Role.

--- src/main/java/br/com/guifroes1984/agenda/domain/entities/Usuario.java
+++ src/main/java/br/com/guifroes1984/agenda/domain/entities/Usuario.java
@@ -1,6 +1,12 @@
 package br.com.guifroes1984.agenda.domain.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
@@ -11,6 +17,10 @@ public class Usuario extends Pessoa {
 
 	private String email;
 	private String password;
+
+	@ManyToMany
+	@JoinTable(name = "TBL_USUARIO_ROLE", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
 	public String getEmail() {
 		return email;
@@ -26,6 +36,14 @@ public class Usuario extends Pessoa {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
 
 }
