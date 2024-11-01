48529cfcfc2524a1f8673154ea55e98b751f5032
guifroes1984 <guilhermefroes26@gmail.com>
2024-11-01 15:23:01 -0300

    implementando ROLES.

---
+++ src/main/java/br/com/guifroes1984/agenda/domain/entities/Role.java
@@ -0,0 +1,60 @@
+package br.com.guifroes1984.agenda.domain.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_ROLE")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	private RoleNome role;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public RoleNome getRole() {
+		return role;
+	}
+
+	public void setRole(RoleNome role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/br/com/guifroes1984/agenda/domain/entities/RoleNome.java
@@ -0,0 +1,7 @@
+package br.com.guifroes1984.agenda.domain.entities;
+
+public enum RoleNome {
+	
+	ROLE_ADMIN, ROLE_OPERADOR
+
+}
